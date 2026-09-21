021877fc2a54fd27e85b61f8bfaa4bad16082187
Alexey <stalker321@yandex.ru>
2026-09-21 12:00:19 +0300

    Kirgizia

---
+++ Foreins/Kirgizia/a_kg17/sources/config/t1.php
@@ -0,0 +1,143 @@
+<?php exit; ?>
+
+[BEGIN LIST]
+
+*number*1
+*options*		1000000000
+*file*			text1_1
+*name*			Консультация
+*end*1
+
+*number*2
+*options*		1000000000
+*file*			text1_2
+*name*			млн кв.м.
+*end*2
+
+*number*3
+*options*		1000000000
+*file*			text1_3
+*name*			Видеокурс
+*end*3
+
+*number*4
+*options*		1000000000
+*file*			text1_4
+*name*			Наименование представителя
+*end*4
+
+*number*5
+*options*		1000000000
+*file*			text1_5
+*name*			Наименование региона
+*end*5
+
+*number*6
+*options*		1000000000
+*file*			text1_6
+*name*			Без учета доставки (ДЛЯ BLOCK1_3)
+*end*6
+
+*number*7
+*options*		1000000000
+*file*			text1_7
+*name*			Домен
+*end*7
+
+*number*8
+*options*		1000000000
+*file*			text1_8
+*name*			Пароль представителя (ДЛЯ PAGE0_32)
+*end*8
+
+*number*9
+*options*		1000000000
+*file*			text1_9
+*name*			Почта на сайте (основная) ДЛЯ page0_20 (форма обратной связи)
+*end*9
+
+*number*10
+*options*		0000000000
+*file*			text1_10
+*name*			Почта на сайте (основная) ДЛЯ page0_46 (ТСС форма обратной связи)
+*end*10
+
+*number*11
+*options*		0000000000
+*file*			text1_11
+*name*			TSS Телефон
+*end*11
+
+*number*12
+*options*		1000000000
+*file*			text1_12
+*name*			Почта на сайте для калькулятора (2 почты, 4 переменных)
+*end*12
+
+*number*13
+*options*		0000000000
+*file*			text1_13
+*name*			empty13
+*end*13
+
+*number*14
+*options*		1000000000
+*file*			text1_14
+*name*			Глобальное наименование
+*end*14
+
+*number*15
+*options*		1000000000
+*file*			text1_15
+*name*			Верхние Фото (для page0_7)
+*end*15
+
+*number*16
+*options*		1000000000
+*file*			text1_16
+*name*			Блок слоган
+*end*16
+
+*number*17
+*options*		1000000000
+*file*			text1_17
+*name*			Ближайший представитель
+*end*17
+
+*number*18
+*options*		0000000000
+*file*			text1_18
+*name*			empty18
+*end*18
+
+*number*19
+*options*		0000000000
+*file*			text1_19
+*name*			empty19
+*end*19
+
+*number*20
+*options*		1000000000
+*file*			text1_20
+*name*			Номер счетчика Я.Метрики
+*end*20
+
+*number*21
+*options*		1000000000
+*file*			text1_21
+*name*			Консультация v2
+*end*21
+
+*number*22
+*options*		1000000000
+*file*			text1_22
+*name*			PHP Вывод WA/Telegram
+*end*22
+
+*number*23
+*options*		1000000000
+*file*			text1_23
+*name*			Номер счетчика Я.Метрики для JavaScript событий
+*end*23
+
+[END LIST]
